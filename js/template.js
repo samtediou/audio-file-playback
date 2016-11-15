0779bccd2a0934f81f78f6a2a2dc8f418ac80e93
David Conaway <dconaway@ean.com>
2016-11-15 11:32:56 -0600

    Refactoring to separate code into css, and js files

---
+++ js/template.js
@@ -0,0 +1,11 @@
+var books = [{"book":"EVANGELIUM SECUNDUM MATTHAEUM", "chapters" : [{"chapter":"1", "audio":"matt01.ogg", "verses":[
+        {"verse":"1", "start":"6", "end": "11.4", "text":"Liber generationis Iesu Christi filii David filii Abraham."},
+        {"verse":"25", "start":"218.2", "end": "224.7", "text":"et non cognoscebat eam, donec peperit filium, et vocavit nomen eius Iesum."}
+      ]},
+      {"chapter":"", "audio":"", "verses":[
+        {"verse":"1", "start":"0  ", "end": "", "text":""},
+        {"verse":"", "start":"", "end": "", "text":""}
+      ]}
+      ]
+    }
+  ];
